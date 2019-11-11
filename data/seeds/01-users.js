2c8abafdc1143d6316524e31aded0ce96dd40e36
Justin Brandley <brandleyj@gmail.com>
2019-11-11 16:37:49 -0600

    added seeds

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,11 +1,9 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex) {
-	// Deletes ALL existing entries
 	return knex("users")
 		.truncate()
 		.then(function() {
-			// Inserts seed entries
 			return knex("users").insert([
 				{ username: "Justin", password: bcrypt.hashSync("pass", 1) },
 				{ username: "John", password: bcrypt.hashSync("pass", 1) },
