1a2b73e12623f2dff21cf604803693461ac12664
Justin Brandley <brandleyj@gmail.com>
2019-11-11 18:03:55 -0600

    stretch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const UserRouter = require("./users/users-router");
+const RestrictedRouter = require("./restricted/restricted-router");
 
 const server = express();
 
@@ -15,5 +16,6 @@ server.get("/", (req, res) => {
 });
 
 server.use("/api", UserRouter);
+server.use("/api/restricted", protected, RestrictedRouter);
 
 module.exports = server;
